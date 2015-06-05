c80dd936e667558868140397c4ddc808414c81c8
bartoszarendt <info@digitalfactory.pl>
2015-06-05 15:37:07 +0200

    Accept cookie on scroll.

--- js/front.js
+++ js/front.js
@@ -5,40 +5,19 @@
 		// handle set-cookie button click
 		$( document ).on( 'click', '.cn-set-cookie', function ( e ) {
 			e.preventDefault();
-
-			var cnTime = new Date(),
-				cnLater = new Date();
-
-			// set expiry time in seconds
-			cnLater.setTime( parseInt( cnTime.getTime() ) + parseInt( cnArgs.cookieTime ) * 1000 );
-
-			// set cookie
-			var cookie_value = $( this ).data( 'cookie-set' ) === 'accept' ? true : false;
-			
-			document.cookie = cnArgs.cookieName + '=' + cookie_value + ';expires=' + cnLater.toGMTString() + ';' + ( cnArgs.cookieDomain !== undefined && cnArgs.cookieDomain !== '' ? 'domain=' + cnArgs.cookieDomain + ';' : '' ) + ( cnArgs.cookiePath !== undefined && cnArgs.cookiePath !== '' ? 'path=' + cnArgs.cookiePath + ';' : '' );
-			
-			// trigger custom event
-			$.event.trigger({
-				type: "setCookieNotice",
-				value: cookie_value,
-				time: cnTime,
-				expires: cnLater
+			$( this ).setCookieNotice( $( this ).data( 'cookie-set' ) );
+		});
+		
+		// handle on scroll
+		if( cnArgs.onScroll ){
+			$(window).on('scroll', function () {
+				if ($( this ).scrollTop() > 100) {
+					// If user scrolls at least 100 pixels
+					$( this ).setCookieNotice( 'accept' );
+					$( this ).off( 'scroll' );
+				}
 			});
-
-			// hide message container
-			if ( cnArgs.hideEffect === 'fade' ) {
-				$( '#cookie-notice' ).fadeOut( 300, function () {
-					$( this ).remove();
-				} );
-			} else if ( cnArgs.hideEffect === 'slide' ) {
-				$( '#cookie-notice' ).slideUp( 300, function () {
-					$( this ).remove();
-				} );
-			} else {
-				$( '#cookie-notice' ).remove();
-			}
-
-		} );
+		}
 
 		// display cookie notice
 		if ( document.cookie.indexOf( 'cookie_notice_accepted' ) === -1 ) {
@@ -49,10 +28,46 @@
 			} else {
 				$( '#cookie-notice' ).show();
 			}
+			
 		} else {
 			$( '#cookie-notice' ).remove();
 		}
 
-	} );
+	});
+	
+	// Set Cookie Notice
+	$.fn.setCookieNotice = function (cookie_value) {
+		
+		var cnTime = new Date(),
+			cnLater = new Date();
+		
+		// set expiry time in seconds
+		cnLater.setTime( parseInt( cnTime.getTime() ) + parseInt( cnArgs.cookieTime ) * 1000 );
+		
+		// set cookie
+		cookie_value = cookie_value === 'accept' ? true : false;
+		document.cookie = cnArgs.cookieName + '=' + cookie_value + ';expires=' + cnLater.toGMTString() + ';' + ( cnArgs.cookieDomain !== undefined && cnArgs.cookieDomain !== '' ? 'domain=' + cnArgs.cookieDomain + ';' : '' ) + ( cnArgs.cookiePath !== undefined && cnArgs.cookiePath !== '' ? 'path=' + cnArgs.cookiePath + ';' : '' );
+		
+		// trigger custom event
+		$.event.trigger({
+			type: "setCookieNotice",
+			value: cookie_value,
+			time: cnTime,
+			expires: cnLater
+		});
+		
+		// hide message container
+		if ( cnArgs.hideEffect === 'fade' ) {
+			$( '#cookie-notice' ).fadeOut( 300, function () {
+				$( this ).remove();
+			} );
+		} else if ( cnArgs.hideEffect === 'slide' ) {
+			$( '#cookie-notice' ).slideUp( 300, function () {
+				$( this ).remove();
+				} );
+		} else {
+			$( '#cookie-notice' ).remove();
+		}
+	};
 
 } )( jQuery );
